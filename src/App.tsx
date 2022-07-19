31145a1de95339ae4f2a41d713e3153677113ee0
Geraldo Luiz <geraldobl58@gmail.com>
2022-07-19 17:50:58 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Main from 'components/Main'
+
+import GlobalStyles from 'styles/global'
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  )
+}
+
+export default App
