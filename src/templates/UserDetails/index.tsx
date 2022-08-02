c5442e873087b159e8cec474573fe8eb23c1a684
Geraldo Luiz <geraldobl58@gmail.com>
2022-08-02 20:00:39 -0300

    :sparkles: Creating logic to delete record

--- src/templates/UserDetails/index.tsx
+++ src/templates/UserDetails/index.tsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { ToolBox } from 'components/ToolBox'
 
 import { Base } from 'templates/Base'
 
+import { Users } from 'services/Users'
+import { LinearProgress, Paper, Typography } from '@mui/material'
+
 export function UserDetails() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [fullName, setFullName] = useState('')
+
   const { id = 'new' } = useParams<'id'>()
 
   const navigate = useNavigate()
@@ -13,9 +20,32 @@ export function UserDetails() {
     console.log('handleSave')
   }
 
-  const handleDelete = () => {
-    console.log('handleDelete')
+  const handleDelete = (id: number) => {
+    if (confirm('Deseja relamente excluir este registro?')) {
+      Users.deleteById(id).then((response) => {
+        if (response instanceof Error) {
+          alert(response.message)
+        } else {
+          alert('Registro excluir com sucesso!')
+          navigate(`/users/`)
+        }
+      })
+    }
   }
+
+  useEffect(() => {
+    if (id !== 'new') {
+      setIsLoading(true)
+      Users.getById(Number(id)).then((response) => {
+        setIsLoading(false)
+        if (response instanceof Error) {
+          navigate(`/users/`)
+        } else {
+          setFullName(response.fullname)
+        }
+      })
+    }
+  }, [id, navigate])
 
   return (
     <Base>
@@ -27,11 +57,16 @@ export function UserDetails() {
         //
         buttonClickSave={handleSave}
         buttonClickSaveAndClose={handleSave}
-        buttonClickDelete={handleDelete}
+        buttonClickDelete={() => handleDelete(Number(id))}
         buttonClickNew={() => navigate(`/users/details/new`)}
         buttonClickBack={() => navigate(`/users/`)}
       />
-      {id}
+      <Paper>
+        <Typography variant="h6" component="div" p={2}>
+          {id === 'new' ? 'Novo Registro' : `Editando o registro [${fullName}]`}
+        </Typography>
+      </Paper>
+      {isLoading && <LinearProgress variant="indeterminate" />}
     </Base>
   )
 }
